f1c1343acfc3ad4988162ad3a3c2491e991327d1
Data Center <p56075607@gmail.com>
2025-05-05 12:49:47 +0800

    change to datacenter setting

--- exe/run_inversion.bat
+++ exe/run_inversion.bat
@@ -1,4 +1,4 @@
-cd C:\Users\Git\TAIP-ERT-Pipeline\scripts
-call C:\Users\B30122\anaconda3\Scripts\activate.bat pg
+cd C:\TAIP-ERT-Pipeline\scripts
+call C:\Users\TAIP_DataCenter\anaconda3\Scripts\activate.bat pg
 call python run_pipeline_inversion.py --config ..\configs\site.yaml
 pause
